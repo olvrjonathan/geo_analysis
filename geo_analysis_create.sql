c25f04bf093b7c6619ca053bf4a7238785545aa8
Marcelo Amaral <marceloamaral386@gmail.com>
2021-11-26 21:31:07 -0300

    Criação do banco de dados

---
+++ geo_analysis_create.sql
@@ -0,0 +1,59 @@
+-- Created by Vertabelo (http://vertabelo.com)
+-- Last modification date: 2021-11-26 19:59:51.927
+
+-- tables
+-- Table: bolsa_familia
+CREATE TABLE bolsa_familia (
+	id int AUTO_INCREMENT NOT NULL,
+    ibge int NOT NULL,
+    UF char(2) NOT NULL,
+    y_m char(7) NOT NULL,
+    qtd_ben_bas int,
+    qtd_ben_var int,
+    qtd_ben_bvj int,
+    qtd_ben_bvn int,
+    qtd_ben_bvg int,
+    qtd_ben_bsp int,
+    CONSTRAINT bolsa_familia_pk PRIMARY KEY (id)
+);
+
+-- Table: cities
+CREATE TABLE cities (
+    id int NOT NULL,
+    UF char(2) NOT NULL,
+    name varchar(50) NOT NULL,
+    geometry geometry NOT NULL,
+    CONSTRAINT cities_pk PRIMARY KEY (id)
+);
+
+-- Table: states
+CREATE TABLE states (
+    code char(2) NOT NULL,
+    state_name varchar(20) NOT NULL,
+    CONSTRAINT states_pk PRIMARY KEY (code)
+);
+
+-- Table: ubs
+CREATE TABLE ubs (
+    cnes int NOT NULL,
+    UF char(2) NOT NULL,
+    name varchar(100) NOT NULL,
+    geometry geometry NOT NULL,
+    CONSTRAINT ubs_pk PRIMARY KEY (cnes)
+);
+
+-- foreign keys
+-- Reference: UBS_states (table: ubs)
+ALTER TABLE ubs ADD CONSTRAINT UBS_states FOREIGN KEY UBS_states (UF)
+    REFERENCES states (code);
+
+-- Reference: bolsa_familia_states (table: bolsa_familia)
+ALTER TABLE bolsa_familia ADD CONSTRAINT bolsa_familia_states FOREIGN KEY bolsa_familia_states (UF)
+    REFERENCES states (code);
+
+-- Reference: cities_states (table: cities)
+ALTER TABLE cities ADD CONSTRAINT cities_states FOREIGN KEY cities_states (UF)
+    REFERENCES states (code);
+
+-- End of file.
+
