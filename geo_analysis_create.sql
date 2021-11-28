785f296d6d6ff0f42c7559ed8440a02280888ce9
Marcelo Amaral <marceloamaral386@gmail.com>
2021-11-28 17:52:21 -0300

    Mais dados e mapas

--- geo_analysis_create.sql
+++ geo_analysis_create.sql
@@ -1,29 +1,32 @@
 -- Created by Vertabelo (http://vertabelo.com)
--- Last modification date: 2021-11-26 19:59:51.927
+-- Last modification date: 2021-11-28 19:35:26.667
 
 -- tables
 -- Table: bolsa_familia
 CREATE TABLE bolsa_familia (
-	id int AUTO_INCREMENT NOT NULL,
+    id int NOT NULL AUTO_INCREMENT,
     ibge int NOT NULL,
     UF char(2) NOT NULL,
     y_m char(7) NOT NULL,
-    qtd_ben_bas int,
-    qtd_ben_var int,
-    qtd_ben_bvj int,
-    qtd_ben_bvn int,
-    qtd_ben_bvg int,
-    qtd_ben_bsp int,
+    qtd_ben_bas int NULL,
+    qtd_ben_var int NULL,
+    qtd_ben_bvj int NULL,
+    qtd_ben_bvn int NULL,
+    qtd_ben_bvg int NULL,
+    qtd_ben_bsp int NULL,
     CONSTRAINT bolsa_familia_pk PRIMARY KEY (id)
 );
 
--- Table: cities
-CREATE TABLE cities (
+-- Table: counties
+CREATE TABLE counties (
     id int NOT NULL,
     UF char(2) NOT NULL,
     name varchar(50) NOT NULL,
     geometry geometry NOT NULL,
-    CONSTRAINT cities_pk PRIMARY KEY (id)
+    population int NOT NULL,
+    idhm double NOT NULL,
+    pib double NULL,
+    CONSTRAINT counties_pk PRIMARY KEY (id)
 );
 
 -- Table: states
@@ -38,7 +41,7 @@ CREATE TABLE ubs (
     cnes int NOT NULL,
     UF char(2) NOT NULL,
     name varchar(100) NOT NULL,
-    geometry geometry NOT NULL,
+    geometry geometry,
     CONSTRAINT ubs_pk PRIMARY KEY (cnes)
 );
 
@@ -51,8 +54,8 @@ ALTER TABLE ubs ADD CONSTRAINT UBS_states FOREIGN KEY UBS_states (UF)
 ALTER TABLE bolsa_familia ADD CONSTRAINT bolsa_familia_states FOREIGN KEY bolsa_familia_states (UF)
     REFERENCES states (code);
 
--- Reference: cities_states (table: cities)
-ALTER TABLE cities ADD CONSTRAINT cities_states FOREIGN KEY cities_states (UF)
+-- Reference: cities_states (table: counties)
+ALTER TABLE counties ADD CONSTRAINT cities_states FOREIGN KEY cities_states (UF)
     REFERENCES states (code);
 
 -- End of file.
